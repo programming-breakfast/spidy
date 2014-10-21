ccee80340d0a853145f063a5fd530d30f58bcb44
Timur Kozmenko <timraell@gmail.com>
2014-10-21 11:51:58 +0700

    feat: introduce erb_lists:pop

---
+++ src/erb_lists.erl
@@ -0,0 +1,24 @@
+%%%-------------------------------------------------------------------
+%%% @author timurkozmenko
+%%% @copyright (C) 2014, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21. окт 2014 10:34
+%%%-------------------------------------------------------------------
+-module(erb_lists).
+-author("timurkozmenko").
+
+%% API
+-export([pop/2]).
+
+pop(Element, List) ->
+  pop(Element, List, [], not_found).
+
+pop(_, [], _, not_found) -> error;
+pop(_, [], Accum, E) -> {E, Accum};
+pop(Element, [Head|List], Accum, _) when Head == Element ->
+  pop(Element, List, Accum, Head);
+pop(Element, [Head|List], Accum, E) ->
+  pop(Element, List, Accum ++ [Head], E).
+
